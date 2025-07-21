ab6946156ea3bea48f66a2e08a9e692bd6e8f437
Egor Medvedev <sapron121@gmail.com>
2025-07-21 15:34:20 +0300

    - read the default file from resources in case the input file is not found

--- src/main/kotlin/io/json/JsonInputReader.kt
+++ src/main/kotlin/io/json/JsonInputReader.kt
@@ -4,6 +4,7 @@ import org.qudisoft.io.InputReader
 import org.qudisoft.model.*
 import org.slf4j.LoggerFactory
 import java.io.File
+import java.io.InputStream
 
 /**
  * TODO: FUTURE DEVELOPMENT - Attributes parser:
@@ -16,9 +17,27 @@ class JsonInputReader : InputReader {
     
     override fun readInput(source: String): PuzzleInput {
         logger.debug("Reading puzzle input from file: $source")
-        val jsonString = File(source).readText()
-        logger.debug("Successfully read ${jsonString.length} characters from input file")
         
-        return jsonParser.parseJsonString(jsonString)
+        val jsonString = readFromFileSystem(source)
+            ?: readFromResources(source)
+            ?: throw IllegalArgumentException("File not found: $source (neither in file system nor in resources)")
+        
+        return jsonString
+            .also { logger.debug("Successfully read ${it.length} characters from input file") }
+            .let(jsonParser::parseJsonString)
     }
+    
+    private fun readFromFileSystem(source: String): String? = 
+        File(source)
+            .takeIf { it.exists() }
+            ?.also { logger.debug("Reading from file system: ${it.absolutePath}") }
+            ?.readText()
+    
+    private fun readFromResources(source: String): String? = 
+        this::class.java.classLoader.getResourceAsStream(source)
+            ?.also { logger.debug("Reading from resources: $source") }
+            ?.use { inputStream ->
+                inputStream.bufferedReader().readText()
+            }
+            .also { if (it == null) logger.debug("File not found in resources") }
 }
